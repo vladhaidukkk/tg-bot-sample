6682f7dec995ec60bd2fba0fa9872349d10cf846
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-07-31 11:07:33 +0300

    create handlers for different message content types

--- bot/main.py
+++ bot/main.py
@@ -13,12 +13,43 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start_command_handler(message: Message) -> None:
-    await message.answer(f"👋 {message.from_user.full_name}")
+    await message.answer(text=f"👋 {message.from_user.full_name}")
 
 
 @dp.message(F.text.regexp(r"^reply:(.+)$").as_("text"))
 async def reply_handler(message: Message, text: Match[str]) -> None:
-    await message.reply(text.group(1))
+    await message.reply(text=text.group(1))
+
+
+@dp.message(F.photo)
+async def photo_handler(message: Message) -> None:
+    photo_id = message.photo[-1].file_id
+    await message.answer_photo(photo=photo_id, caption="Thank you for the photo. It looks nice 😁")
+
+
+@dp.message(F.sticker)
+async def sticker_handler(message: Message) -> None:
+    sticker_id = message.sticker.file_id
+    await message.answer_sticker(sticker=sticker_id)
+    await message.answer(text="This sticker is amazing 🤩")
+
+
+@dp.message(F.animation)
+async def animation_handler(message: Message) -> None:
+    animation_id = message.animation.file_id
+    await message.answer_animation(animation=animation_id, caption="I like this animation 🥰")
+
+
+@dp.message(F.document)
+async def document_handler(message: Message) -> None:
+    document_id = message.document.file_id
+    await message.answer_document(document=document_id, caption="Thank you for the document. It is interesting 🧐")
+
+
+@dp.message(F.voice)
+async def voice_handler(message: Message) -> None:
+    voice_id = message.voice.file_id
+    await message.answer_voice(voice=voice_id, caption="Sounds interesting 🤔")
 
 
 @dp.message()
