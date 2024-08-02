1213118e282e6dc801c599df5eb0de9ce92c5daf
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-08-02 10:28:13 +0300

    add keyboard btns for poll and quiz

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,16 +1,23 @@
 from enum import StrEnum
 
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.enums.poll_type import PollType
+from aiogram.types import KeyboardButton, KeyboardButtonPollType, ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 class MainKeyboardButton(StrEnum):
     INVITE = "🤝 Invite Friend"
     CONTACT = "📞 Share Contact"
+    POLL = "📊 Create Poll"
+    QUIZ = "🤔 Create Quiz"
 
 
 def build_main_keyboard() -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
     builder.add(KeyboardButton(text=MainKeyboardButton.INVITE))
     builder.add(KeyboardButton(text=MainKeyboardButton.CONTACT, request_contact=True))
+    builder.add(
+        KeyboardButton(text=MainKeyboardButton.POLL, request_poll=KeyboardButtonPollType(type=PollType.REGULAR))
+    )
+    builder.add(KeyboardButton(text=MainKeyboardButton.QUIZ, request_poll=KeyboardButtonPollType(type=PollType.QUIZ)))
     return builder.adjust(2).as_markup(resize_keyboard=True, input_field_placeholder="Click on a button")
